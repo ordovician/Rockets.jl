1e1ec7c0a73e96c1ed93f4db6b3da30f6f7d05c6
Erik Engheim <erik.engheim@mac.com>
2018-12-31 01:28:16 +0100

    Improving plotting functionality
    Makes it easier to export data for plotting in Omni GraphSketcher

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,6 +1,7 @@
 using DelimitedFiles
+using InteractiveUtils
 
-export export_table
+export export_table, plot
 
 "Will copy matrix `M` to clipboard in a format which can be pasted into Apple Numbers"
 function export_table(M)
@@ -9,3 +10,29 @@ function export_table(M)
     s = String(take!(io))
     clipboard(s)
 end
+
+function plot(f::Function, xs)
+    ys = f.(xs)
+    if eltype(ys) <: Tuple
+        rows = length(ys)
+        if rows > 0
+            cols = length(ys[1])
+            M = zeros((rows, cols + 1))
+            M[:, 1] = xs
+            for (i, tuple) in enumerate(ys)
+                for (j, x) in enumerate(tuple)
+                    M[i, j + 1] = x
+                end
+            end
+            export_table(M)
+            M
+        else
+            error("No rows")
+        end 
+    else
+        M = hcat(xs, ys)
+        export_table(M)
+        M
+    end
+end
+
